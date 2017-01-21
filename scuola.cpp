c8748c0f090bb36489650d93a94159e749f2b4c4
Billie99 <billiearmstrong1886@gmail.com>
2017-01-21 01:28:39 +0100

    Create scuola.cpp

---
+++ scuola.cpp
@@ -0,0 +1,39 @@
+#include <string.h>
+#include "scuola.h"
+#include <iostream>
+#include <unistd.h>
+
+using std::cout;
+
+scuola::scuola()
+{
+ this->alunno = new char[1];
+ this->alunno[0] = '\0';
+}
+
+scuola::scuola(const char str[])
+{
+ this->set(str);
+}
+
+void scuola::set(const char* str)
+{
+ int len = strlen(str);
+ if(this->alunno!=NULL) delete[] this-> alunno;
+ this->alunno = new char[len];
+ strncpy(this->alunno, str, len);
+ this->alunno[len] = '\0';
+}
+
+scuola::~scuola()
+{
+ if(this->alunno!=NULL) delete[] this->alunno;
+ sleep(3);
+ cout << "Il distruttore e' stato attivato \n";
+}
+
+char* scuola::get()
+{
+ return this->alunno;
+}
+    
