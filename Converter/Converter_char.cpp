1f9c0fa64fd9bbea544e2329bb8605136ea3a2d4
Billie99 <billiearmstrong1886@gmail.com>
2017-01-27 00:42:30 +0100

    Create Converter_char.cpp

---
+++ Converter/Converter_char.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ char a;
+
+ cout << "inserisci un carattere \t";
+ cin >> a;
+
+ cout << "La sua conversione in binario e' : " 
+ <<(bool)(a & (1<<7)) //10000000
+ <<(bool)(a & (1<<6)) //01000000
+ <<(bool)(a & (1<<5)) //00100000
+ <<(bool)(a & (1<<4)) //00010000
+ <<(bool)(a & (1<<3)) //00001000
+ <<(bool)(a & (1<<2)) //00000100
+ <<(bool)(a & (1<<1)) //00000010
+ <<(bool)(a & (1<<0)) //00000001
+ << endl;
+
+ return 0;
+}
