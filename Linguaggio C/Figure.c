006dd5b30abb8c0faa7026c01672ac362040b50a
Billie99 <billiearmstrong1886@gmail.com>
2017-02-01 14:41:09 +0100

    Aggiustamenti

--- Linguaggio C/Figure.c
+++ Linguaggio C/Figure.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/* Programma che stampa delle figure */
+
+ /* Per eseguire questo programma digitare da terminale
+   gcc -c Fullname.c -o Fullname.o
+   gcc Fullname.o -o Fullname */
+
+
 int main()
 {
  printf("********   **** \n");
