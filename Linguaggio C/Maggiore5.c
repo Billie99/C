3acbbb7e4beea49aeabd05daec89bc28719cf8e1
Billie99 <billiearmstrong1886@gmail.com>
2017-02-03 01:41:52 +0100

    Aggiustamenti

--- Linguaggio C/Maggiore5.c
+++ Linguaggio C/Maggiore5.c
@@ -27,14 +27,22 @@ int main()
  printf("Inserisci un numero ");
  scanf("%d", &numero5);
 
- /* Determinazione del massimo */
  massimo = numero1;
 
- if(numero2 > massimo) massimo = numero2;
- if(numero3 > massimo) massimo = numero3;
- if(numero4 > massimo) massimo = numero4;
- if(numero5 > massimo) massimo = numero5;
- 
+ if(numero1 == numero2 && numero2 == numero3 && numero3 == numero4 && numero4 == numero5)
+{
+ printf("I numeri sono uguali\n");
+ return 0; 
+}
+ else{
+ /* Determinazione del massimo */
+
+ if(numero2 > massimo){ massimo = numero2; }
+ if(numero3 > massimo){ massimo = numero3; }
+ if(numero4 > massimo){ massimo = numero4; }
+ if(numero5 > massimo){ massimo = numero5; }
+} 
+
  printf("Il massimo e': %d\n", massimo);
 
  return 0;
