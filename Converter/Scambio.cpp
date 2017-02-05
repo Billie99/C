212cdb1f002d688e938c03e0a81ccd8accff6c24
Billie99 <billiearmstrong1886@gmail.com>
2017-02-05 02:06:05 +0100

    Nuovo metodo di scambio

--- Converter/Scambio.cpp
+++ Converter/Scambio.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <unistd.h>
 
+/* Programma che esegue lo scambio tra due valori */ 
+
 using namespace std;
 
 int main()
 {
+/* Dichiarazione di variabili */ 
  int a;
  int b;
 
+/* Input dei valori */
  cout << "Inserisci a \n";
  cin >> a;
 
@@ -16,12 +20,14 @@ int main()
 
  cout << "I valori sono: " << a << " e " << b << endl;
 
+/* Scambio con il metodo xor */
  a ^= b;
  b ^= a;
  a ^= b;
 
  sleep(2);
 
+/* Stampa dei valori */
  cout << "I valori scambiati sono: " << a << " e " << b << endl;
 
  return 0;
