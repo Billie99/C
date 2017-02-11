155665539bcc60a420b4067473596f1b29d80c9a
Billie99 <billiearmstrong1886@gmail.com>
2017-02-11 14:58:46 +0100

    Update Fibonacci.cpp

--- Bitwise/Fibonacci.cpp
+++ Bitwise/Fibonacci.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
+/* Questo programma esegue l'algoritmo di Fibonacci */
+
+
 using namespace std;
 
 int main()
 {
+
+/* Inizzializziamo le nostre variabili */
+ 
  int n;
  int primo_numero = 0;
  int secondo_numero = 1;
@@ -12,6 +18,8 @@ int main()
  cout << "Inserisci il numero che vuoi trasformare \n";
  cin >> n;
 
+/* Eseguiamo un ciclo for per contare un tot numero di volte */ 
+ 
  for(int i = 0; i < n; i++)
 {
  if(i <= 1)
@@ -20,11 +28,14 @@ int main()
 }
  else
 {
+ /* Successione di numeri in cui ciascun numero e' la somma dei due precedenti */
+  
  somma = primo_numero + secondo_numero;
  primo_numero = secondo_numero;
  secondo_numero = somma;
 }
-
+/* Stampa della successione */
+  
  cout << somma << endl;
 
 }
