d53719281e6195065d99e9910d3e44ffff110912
Billie99 <billiearmstrong1886@gmail.com>
2017-02-12 02:41:50 +0100

    Create a new file

---
+++ Aritmetica/Aritmetica_modulare.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int a;
+ int i;
+ int c;
+ int z = 0;
+
+ cout << "Associamo ai giorni della settimana dei numeri: Lunedi,Martedi.. 1,2,3 \n";
+ cout << "Inserisci il numero da determinare (ad esempio fra 10 giorni che giorno sara') \n";
+ cin >> a;
+
+ cout << "Inserisci il giorno in numeri \n";
+ cin >> c;
+
+ if(a == 0 && c == 0)
+{
+ cout << "Il numero di giorni e' " << a << endl;
+}
+ else
+{
+ z = (c + a) % 7;
+}
+ cout << "Fra " << a << " giorni sara' " << z << endl;
+
+ return 0;
+
+}
