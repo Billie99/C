56e8a327cb6cec30a5646ff69dcc49f6adc204b0
Billie99 <billiearmstrong1886@gmail.com>
2017-03-03 10:17:52 +0100

    Update SumVett.c

--- Linguaggio C/SumVett.c
+++ Linguaggio C/SumVett.c
@@ -2,29 +2,37 @@
 
 int main()
 {
-   int numbers[3];
-   int indice;
-   int somma = 0;
-   double media = 0;
-
-   printf("Inserisci tre valori interi\n");
-
-   for(indice = 0; indice < 3; indice ++)
-   {
-     printf("? ");
-     scanf("%d", &numbers[indice]);
-     somma = somma + numbers[indice];
-   }
-    printf("Dati iniziali: \n");
-
-    printf("numbers[0] = %d\n", numbers[0]);
-    printf("numbers[1] = %d\n", numbers[1]);
-    printf("numbers[2] = %d\n", numbers[2]);
-
-    printf("Somma: %d\n", somma);
-
-    media = somma / indice;
-    printf("Media: %f\n", media);
- 
-    return 0;
+  int numbers[3];
+  int somma = 0;
+  double media = 0;
+  
+  printf("Somma e Media\n");
+  printf("\n");
+  
+  printf("Inserisci tre valori\n");
+  printf("\n");
+    
+  printf("? ");
+  scanf("%d", &numbers[0]);
+  printf("? ");
+  scanf("%d", &numbers[1]);
+  printf("? ");
+  scanf("%d", &numbers[2]);
+  printf("\n");
+ 	
+  printf("Dati iniziali:\n");
+  
+  printf("numbers[0] = %d\n", numbers[0]);
+  printf("numbers[1] = %d\n", numbers[1]);
+  printf("numbers[2] = %d\n", numbers[2]);
+  printf("\n");
+  
+  somma = numbers[0] + numbers[1] + numbers[2];
+  printf("Somma: %d\n", somma);
+  
+  media = somma / 3.0;
+  printf("Media: %lf\n", media);
+  
+  system("PAUSE");
+  return 0;
 }
