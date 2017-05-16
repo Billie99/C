2775f427cb294fd15c707a9d9851bdf38f9d0d9e
Billie99 <billiearmstrong1886@gmail.com>
2017-05-16 21:14:35 +0200

    Update TheSum.c

--- no_term/TheSum.c
+++ no_term/TheSum.c
@@ -7,9 +7,6 @@ int main()
 int num = 0;
 int numero = 0;
 
-while(1)
-{
-
 FILE *f = fopen("sum.dat", "r");
 
 if (!f)
@@ -22,28 +19,22 @@ if (!f)
 printf("Lettura dal file sum.dat in corso ... \n");
 fscanf(f, "%d", &numero);
 fclose(f);
-sleep(2);
 
 printf("Valore corrente: %d\n", numero);
-sleep(2);
 
 printf("Inserisci il valore da sommare: ");
 scanf("%d", &num);
-sleep(2);
 
 numero = numero + num;
 
 printf("Il nuovo valore e': %d\n", numero);
-sleep(2);
 printf("Salvataggio nel file sum.dat in corso ...\n");
-sleep(2);
 printf("\n");
 
 f = fopen("sum.dat", "w");
 fprintf(f, "%d\n", numero);
 fclose(f);
 
-}
 return 0;
 
 }
