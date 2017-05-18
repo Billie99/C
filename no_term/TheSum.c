af90ff5b4805e44e82ec10aa9cd6838aacf70a05
Billie99 <billiearmstrong1886@gmail.com>
2017-05-18 17:41:13 +0200

    Aggiustamenti

--- no_term/TheSum.c
+++ no_term/TheSum.c
@@ -4,6 +4,11 @@
 int main()
 {
 
+struct timespec tempo;
+
+tempo.tv_sec = 2;
+tempo.tv_nsec = 0;
+
 int num = 0;
 int numero = 0;
 
@@ -12,24 +17,29 @@ FILE *f = fopen("sum.dat", "r");
 if (!f)
 {
  printf("Impossibile aprire il file, il file non esiste \n");
-
- return 0;
+ nanosleep(&tempo, NULL);
+ printf("Verra creato un nuovo file con il valore immesso dall' utente\n");
+ nanosleep(&tempo, NULL);
 }
 
-printf("Lettura dal file sum.dat in corso ... \n");
-fscanf(f, "%d", &numero);
-fclose(f);
+else
+{
+ printf("Lettura dal file sum.dat in corso ... \n");
+ fscanf(f, "%d", &numero);
+ fclose(f);
+ nanosleep(&tempo, NULL);
+}
 
 printf("Valore corrente: %d\n", numero);
-
+nanosleep(&tempo, NULL);
 printf("Inserisci il valore da sommare: ");
 scanf("%d", &num);
 
 numero = numero + num;
 
 printf("Il nuovo valore e': %d\n", numero);
+nanosleep(&tempo, NULL);
 printf("Salvataggio nel file sum.dat in corso ...\n");
-printf("\n");
 
 f = fopen("sum.dat", "w");
 fprintf(f, "%d\n", numero);
