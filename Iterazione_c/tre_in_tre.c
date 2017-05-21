05f4a6f40350ff805e0dd81193ecda705054998f
Billie99 <billiearmstrong1886@gmail.com>
2017-05-21 12:28:56 +0200

    Aggiustamenti

--- Iterazione_c/tre_in_tre.c
+++ Iterazione_c/tre_in_tre.c
@@ -1,21 +1,38 @@
 /* 3_in_3.c */
+
 #include <stdio.h>
+#include <time.h>
 
 int main()
 {
 
  int numero = 0;
- int conta = 30;
+ int conta = 0;
 
- puts("Inserisci il numero di partenza");
- scanf("%d\n", &numero);
+ struct timespec tempo;
+
+ tempo.tv_sec = 1; // 1 secondo
+ tempo.tv_nsec = 0;
+
+ printf("Inserisci il numero di partenza ");
+ scanf("%d", &numero);
+
+ printf("Inserisci il numero arrivo ");
+ scanf("%d", &conta);
+
+ printf("\n");
 
  printf("Numero di partenza: %d\n", numero);
  printf("\n");
 
- while( numero < conta )
+ printf("Viaa!!\n");
+ nanosleep( &tempo, NULL );
+
+ while( numero <= conta )
 {
  printf("%d\n", numero);
+
+ nanosleep( &tempo, NULL );
 
  numero = numero + 3;
 }
