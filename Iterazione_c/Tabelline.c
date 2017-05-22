b63d9c4bc248ca2dc4e6622f9723bef64a8403ab
alunno <alunno@lab03-03>
2017-05-22 12:00:05 +0200

    Aggiustamenti

--- Iterazione_c/Tabelline.c
+++ Iterazione_c/Tabelline.c
@@ -6,12 +6,24 @@ int main()
 {
 
  int numero = 0;
+ int j;
+ int risultato = 0;
 
  while ( numero < 11 )
 {
- printf("%d\t", numero);
- numero = numero + 1;
+ j = 0;
 
+ while ( j < 11 )
+{
+
+ risultato = numero * j;
+
+ printf("%d\t", risultato);
+ j = j + 1;
+}
+
+printf("\n");
+numero = numero + 1;
 }
 
  return 0;
