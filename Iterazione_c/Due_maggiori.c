3cbcd48fec8427b7e177bbd224c14267a7c8f23a
Billie99 <billiearmstrong1886@gmail.com>
2017-05-25 00:48:35 +0200

    Aggiustamenti

---
+++ Iterazione_c/Due_maggiori.c
@@ -0,0 +1,52 @@
+/* Due_maggiori.c */
+
+#include <stdio.h>
+
+int main()
+{
+
+ int conta = 0;
+ int numero;
+ int massimo = 0;
+ int minimo = 0;
+
+ // input primo valore
+ printf("Inserisci i primi numeri ");
+ scanf("%d", &numero);
+
+ massimo = numero;
+
+ // input secondo valore
+ printf("Inserisci i primi numeri ");
+ scanf("%d", &numero);
+
+ if ( numero > massimo )
+{
+ minimo = massimo; // si effettua una scalatura
+ massimo = numero;
+}
+
+ else
+{
+ minimo = numero;
+}
+
+ while ( conta < 8 )
+{
+ printf("Inserisci i numeri ");
+ scanf("%d", &numero);
+
+ if ( numero > massimo ) { minimo = massimo;
+
+      massimo = numero;
+}
+
+ conta = conta + 1;
+
+}
+
+ printf("Il massimo1 e': %d\n", massimo);
+ printf("Il massimo2 e': %d\n", minimo);
+
+ return 0;
+}
