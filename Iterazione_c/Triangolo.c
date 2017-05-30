ecf80d930c048bd6ab3be4bd924aba94ff4703c7
Billie99 <billiearmstrong1886@gmail.com>
2017-05-30 20:09:50 +0200

    Aggiunto nuovo file

---
+++ Iterazione_c/Triangolo.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int main()
+{
+
+ int numero;
+ int colonna;
+ int righe;
+
+ char carattere;
+
+ printf("Disegna con ");
+ scanf("%c", &carattere);
+
+
+ do {
+
+ printf("Inserisci il numero del lato ");
+ scanf("%d", &numero);
+
+
+ if ( numero < 1 || numero > 20 )
+ {
+
+ printf("Valore fuori dai limiti ");
+
+ }
+
+ } while ( numero < 1 || numero > 20 );
+
+
+ putchar('\n');
+ colonna = 0;
+
+ while ( colonna < numero )
+
+ {
+
+ righe = 0;
+
+ while ( righe < ( numero - colonna ) )
+
+ {
+
+   righe = righe + 1 ;
+
+   putchar(carattere);
+
+ }
+
+  putchar('\n');
+
+  colonna = colonna + 1;
+
+ }
+
+ return 0;
+}
