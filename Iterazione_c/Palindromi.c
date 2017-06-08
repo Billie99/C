6e7c398ac91785ce1357094f66c90856a6164e53
Billie99 <billiearmstrong1886@gmail.com>
2017-06-08 19:50:50 +0200

    Palindromi

---
+++ Iterazione_c/Palindromi.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+#define True 1
+#define False 0
+
+
+int main()
+{
+  int numero;
+  int conta;
+  int cifra1;
+  int cifra2;
+  int cifra3;
+  int cifra4;
+  int cifra5;
+  int verita;
+
+ do {
+	printf("Inserisci il numero ");
+ 	scanf("%d", &numero);
+
+	if ( numero < 0 )
+	{
+	   printf("Non sono ammessi i numeri negativi \n");
+	}
+
+	else if ( numero > 99999 )
+	{
+	   printf("Inserire solo numeri di 5 cifre \n");
+	}
+
+
+ } while ( numero < 0 || numero > 99999 );
+
+ // estrazione della prima cifra
+ cifra1 = numero % 10;
+ numero = numero / 10;
+
+ // estrazione della seconda cifra
+ cifra2 = numero % 10;
+ numero = numero / 10;
+
+ // estrazione terza cifra
+ cifra3 = numero % 10;
+ numero = numero / 10;
+
+ // estrazione quarta cifra
+ cifra4 = numero % 10;
+ numero = numero / 10;
+
+ // estrazione quinta cifra
+ cifra5 = numero % 10;
+ numero = numero / 10;
+
+ // Verifica se e' palindromo
+ if ( cifra5 == cifra1 && cifra4 == cifra2 )
+ {
+    verita = True;
+ }
+
+ else
+ {
+   verita = False;
+ }
+
+
+ if (verita)
+ {
+    printf("Il numero inserito e' palindromo \n");
+ }
+
+ else
+ {
+    printf("Il numero non e' palindromo \n");
+ }
+
+ return 0;
+}
